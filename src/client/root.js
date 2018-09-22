7b24ea5d4a44b25fc0c7e4b2c1d90138f4edaaf4
kangtaehee <rkdxogml5768@naver.com>
2018-09-22 15:25:10 +0900

    studyroom side section

---
+++ src/client/root.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import App from "../shared/App";
+import {BrowserRouter} from "react-router-dom";
+
+
+class Root extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        );
+    };
+}
+
+
+export default Root;
