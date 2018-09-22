d97c1201c08ae5cb20ac9d3ee011276d82d50f7a
마수현 <bigmsh11@gmail.com>
2018-09-22 11:46:51 +0900

    API 추가 및 카테고리 목록 구현

--- src/components/studyCategory/studyCategory.js
+++ src/components/studyCategory/studyCategory.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import "./studyCategory.scss"
 import {NavLink} from "react-router-dom";
+import Api from '../../api'
 
 const StudyCategories = [
     {
@@ -30,15 +31,13 @@ const StudyCategories = [
     }
 ];
 
-function GetStudyCategories() {
-    //API: [GET] 카테고리 리스트
-
-    let StudyCategoryList = StudyCategories.map((StudyCategory, i) =>
+function GetStudyCategories(props) {
+    let StudyCategoryList = props.categories.map((StudyCategory, i) =>
 
         <li key={i}>
-            <NavLink exact to={{pathname: '/study/' + StudyCategory.categoryID}}>
+            <NavLink exact to={{pathname: '/study/' + StudyCategory.id}}>
                 <div className="category-card">
-                    <div className="category-title">{StudyCategory.categoryLevel2}</div>
+                    <div className="category-title">{StudyCategory.name}</div>
                 </div>
             </NavLink>
         </li>
@@ -51,6 +50,26 @@ function GetStudyCategories() {
 
 
 class StudyCategory extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            categories: []
+        };
+
+        //API: [GET] 카테고리 리스트
+        var that = this;
+        Api.get('/categories').then(function(res) {
+            let categories = [];
+            res.data.forEach(element => {
+                categories.push(element);
+            });
+            that.setState({
+                categories: categories
+            })
+        });
+    }
+
     render() { //class 형태의 Component에 필수로 있어야하는 함수S_내부에서 JSX 리턴
         return (
             <div className="Container StudyCatogory">
@@ -58,7 +77,7 @@ class StudyCategory extends Component {
                 <div className="title">어떤 주제로 스터디해볼까요?</div>
                 <div className="list-title">스터디 카테고리 리스트</div>
 
-                <GetStudyCategories/>
+                <GetStudyCategories categories={this.state.categories}/>
             </div>
         );
     }
