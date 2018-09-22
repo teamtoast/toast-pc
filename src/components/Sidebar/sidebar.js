d6cb5683963c4a51ad409e976d921c10ef5b1465
kangtaehee <rkdxogml5768@naver.com>
2018-09-22 15:02:35 +0900

    fixed sidebar - >show curr menu

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -2,47 +2,69 @@ import React, {Component} from 'react';
 import './sidebar.scss'
 import {NavLink} from "react-router-dom";
 
-class Sidebar extends Component{
+class Sidebar extends Component {
+
     render() { //class 형태의 Component에 필수로 있어야하는 함수_내부에서 JSX 리턴
+        const currentPath = window.location.pathname;
+
         return (
-            <ul className="menu-list">
-                <li>
-                    <NavLink exact to="/" activeClassName="active" >
-                        <img alt="home"
-                             src={require('./img/ic-manu-w@3x.png')}
-                             className="ic_Manu_W"/>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/" activeClassName="active">
-                        <img alt="home"
-                             src={require('./img/ic-home@3x.png')}
-                             className="ic_Home"/>
-                        <br/>
-                        <div className="menu-font">Home</div>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/study" activeClassName="active">
-                        <img alt="study"
-                             src={require('./img/ic-study@3x.png')}
-                             className="ic_Study"/>
-                        <br/>
-                        <div className="menu-font">Study</div>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/level-test" activeClassName="active">
-                        <img alt="level"
-                             src={require('./img/ic-level-test@3x.png')}
-                             className="ic_LevelTest"/>
-                        <br/>
-                        <div className="menu-font">Level Test</div>
+            <div className="Sidebar">
+                <div className={"bar" + ((currentPath === '/')? " bar-home":
+                    ((currentPath.includes('/study'))? " bar-study" : " bar-leveltest"))}/>
+
+                <ul className="menu-list">
+                    <li>
+                        <NavLink exact to="/" activeClassName="active">
+                            <img alt="home"
+                                 src={require('./img/ic-manu-w@3x.png')}
+                                 className="ic_Manu_W"/>
                         </NavLink>
-                </li>
-            </ul>
+                    </li>
+                    <li>
+                        <NavLink to="/" activeClassName="active">
+                            {(currentPath === '/' ?
+                                <img alt="home"
+                                     src={require('./img/ic-home-active@3x.png')}
+                                     className="ic_Home"/>
+                                : <img alt="home"
+                                       src={require('./img/ic-home@3x.png')}
+                                       className="ic_Home"/>)}
 
-
+                            <br/>
+                            <div className={"menu-font" + (currentPath === '/' ? " active" : "")}>
+                                Home
+                            </div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/study" activeClassName="active">
+                            {(currentPath.includes('/study') ?
+                                <img alt="study"
+                                     src={require('./img/ic-study-active@3x.png')}
+                                     className="ic_Study"/>
+                                : <img alt="study"
+                                       src={require('./img/ic-study@3x.png')}
+                                       className="ic_Study"/>)}
+                            <br/>
+                            <div className={"menu-font" + (currentPath.includes('/study') ? " active" : "")}>Study</div>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/level-test" activeClassName="active">
+                            {(currentPath === '/level-test' ?
+                                <img alt="level"
+                                     src={require('./img/ic-level-test-active@3x.png')}
+                                     className="ic_LevelTest"/>
+                                : <img alt="level"
+                                       src={require('./img/ic-level-test@3x.png')}
+                                       className="ic_LevelTest"/>)}
+                            <br/>
+                            <div className={"menu-font" + (currentPath === '/level-test' ? " active" : "")}>Level Test
+                            </div>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,12 +9,8 @@ class Header extends Component {
                 <div className="logo">
                     <NavLink exact to="/" activeClassName="active">
                         <img alt="logo"
-                             src={require('./img/logo-nav.png')}
-                             srcSet={`
-                         ${require('./img/logo-nav.png')} 300w,
-                         ${require('./img/logo-nav@2x.png')} 768w,
-                         ${require('./img/logo-nav@3x.png')} 1280w`}
-                             className="Logo_Nav"></img>
+                             src={require('./img/logo-nav@3x.png')}
+                             className="Logo_Nav"/>
                     </NavLink>
                 </div>
 
@@ -22,13 +18,8 @@ class Header extends Component {
                 <div className="login-btn">
                     <NavLink to="/login" activeClassName="active">
                         <img alt="logo"
-                             src={require('./img/button-login.png')}
-                             srcSet={`
-                         ${require('./img/button-login.png')} 300w,
-                         ${require('./img/button-login@2x.png')} 768w,
-                         ${require('./img/button-login@3x.png')} 1280w`}
-                             className="Button_Login">
-                        </img>
+                             src={require('./img/button-login@3x.png')}
+                             className="Button_Login"/>
                         </NavLink>
                 </div>
             </div>
