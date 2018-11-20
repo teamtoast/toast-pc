9f1c2d6aab778bb3b0e43187cce0a4a522e1fe6d
kangtaehee <rkdxogml5768@naver.com>
2018-11-20 11:36:44 +0900

    feedback

--- src/components/feedback/feedbackContent.js
+++ src/components/feedback/feedbackContent.js
@@ -4,37 +4,42 @@ import './feedback.scss'
 import Api from "../../api";
 
 export function GrammerFeedbackList(props) {
-    let grammarFeedbacks = props.grammarFeedbacks;
-    let feedbackResult=[];
+    let grammarFeedbacks;
+    let feedbackResult = [];
 
-    for (let i = 0; i < grammarFeedbacks.length; i++) {
-        let expression = grammarFeedbacks[i].expression;
-        let grammarFeedback = grammarFeedbacks[i].grammarFeedback;
+    if(props.grammarFeedbacks) {
+        grammarFeedbacks=props.grammarFeedbacks;
+        for (let i = 0; i < grammarFeedbacks.length; i++) {
+            let expression = grammarFeedbacks[i].expression;
+            let grammarFeedback = grammarFeedbacks[i].grammarFeedback;
 
-        let startIdx = 0;
-        let offsetBefore=0,lengthBefore=0;
-        if (grammarFeedback) {
-            let feedbackPart = grammarFeedback.map((element, idx) => {
-                startIdx = offsetBefore + lengthBefore;
-                offsetBefore = element.offset;
-                lengthBefore = element.length;
-                return (
-                    <li className={"feedback-sentence"}>
-                        {(idx===0)? <p className={"feedback-idx"}>{i+1}.</p> :null}
-                        <p>{expression.substring(startIdx, element.offset)}</p>
-                        <p className={"bad-word bad-word-" + (element.type)}>{expression.substr( element.offset, element.length)}</p>
-                        <p className={"better-word better-word-" + (element.type)}>{element.better[0]}</p>
-                        { (grammarFeedback.length === idx+1)?
-                            <p>{expression.substring(offsetBefore+lengthBefore, expression.length)}</p>: null}
-                    </li>
+            let startIdx = 0;
+            let offsetBefore = 0, lengthBefore = 0;
+            if (grammarFeedback) {
+                let feedbackPart = grammarFeedback.map((element, idx) => {
+                        startIdx = offsetBefore + lengthBefore;
+                        offsetBefore = element.offset;
+                        lengthBefore = element.length;
+                        return (
+                            <li className={"feedback-sentence"}>
+                                {(idx === 0) ? <p className={"feedback-idx"}>{i + 1}.</p> : null}
+                                <p>{expression.substring(startIdx, element.offset)}</p>
+                                <p className={"bad-word bad-word-" + (element.type)}>{expression.substr(element.offset, element.length)}</p>
+                                <p className={"better-word better-word-" + (element.type)}>{element.better[0]}</p>
+                                {(grammarFeedback.length === idx + 1) ?
+                                    <p>{expression.substring(offsetBefore + lengthBefore, expression.length)}</p> : null}
+                            </li>
 
+                        );
+                    }
                 );
-                }
-            );
-            feedbackResult.push(feedbackPart);
-        }
+                feedbackResult.push(feedbackPart);
+            }
 
+        }
     }
+
+
 
     return (
         <ul className={"feedbackResult"}>{feedbackResult}</ul>
@@ -43,109 +48,59 @@ export function GrammerFeedbackList(props) {
 
 class FeedbackContent extends Component {
 
-    state = {
-        feedbackTotal: {
-            "grammarFeedbacks": [
-                {
-                    "expression": "Where is nearest hospital",
-                    "grammarFeedback": [
-                        {
-                            "offset": 6,
-                            "length": 10,
-                            "bad": "is nearest",
-                            "better": [
-                                "is the nearest"
-                            ],
-                            "type": "grammar"
-                        }
-                    ]
-                },
-                {
-                    "expression": "My mother are a doctor, but my father is a angeneer",
-                    "grammarFeedback": [
-                        {
-                            "offset": 10,
-                            "length": 3,
-                            "bad": "are",
-                            "better": [
-                                "is"
-                            ],
-                            "type": "grammar"
-                        },
-                        {
-                            "offset": 41,
-                            "length": 1,
-                            "bad": "a",
-                            "better": [
-                                "an"
-                            ],
-                            "type": "spelling"
-                        },
-                        {
-                            "offset": 43,
-                            "length": 8,
-                            "bad": "angeneer",
-                            "better": [
-                                "engineer",
-                                "engender"
-                            ],
-                            "type": "spelling"
-                        }
-                    ]
-                },
-                {
-                    "expression": "What is the nearest hospital"
-                }
-            ],
-            "recommendSentences": [
-                {
-                    "question": "Where is nearest hospital",
-                    "answer": "It’s 500 m far from my house."
-                },
-                {
-                    "question": "My mother are a doctor, but my father is a angeneer",
-                    "answer": "It adds structure to our social lives and connects us with our families and backgrounds."
-                },
-                {
-                    "question": "What is the nearest hospital",
-                    "answer": "Museums are an integral part of any country’s history, and they keep history alive."
-                }
-            ],
-            "poorPronunciation": ["nearest", "hospital"],
-            "pronunciationScore": "Excellent",
-            "grammarScore": "Excellent",
-            "wordScore": "Excellent",
-            "expressionScore": "Great"
-        }
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            feedbackTotal: {},
+            feedback: {},
+            missedPronunciationList: [],
+            recommendSentList: []
+        };
 
     }
 
-    render() {
-        let feedback = this.props.feedback;
-        let missedPronunciationList = this.state.feedbackTotal.poorPronunciation.map((word, i) =>
-            <li className="missed-pronunciation">
-                <p>{word}</p>
-                <img src={require('./img/button_sound.png')}
-                     className="btn-sound" alt=""/>
-            </li>
-        );
 
-        let recommendSentList = this.state.feedbackTotal.recommendSentences.map((qna, i) =>
-            <li className="recommend-sentence">
-                <p>Q. {qna.question}</p>
-                <p>A. {qna.answer}</p>
-            </li>
-        );
+    render() {
+
+        var that = this;
+        let path = "home/ubuntu/speeches_saved";
+        Api.getParam('/feedback/getAllFeedback', path).then(function (res) {
+            that.setState({
+                feedbackTotal: res.data
+            });
+        });
+        if (this.state.feedbackTotal) {
+            this.state.feedback = this.props.feedback;
+            if(this.state.feedbackTotal.poorPronunciation) {
+                this.state.missedPronunciationList = this.state.feedbackTotal.poorPronunciation.map((word, i) =>
+                    <li className="missed-pronunciation">
+                        <p>{word}</p>
+                        <img src={require('./img/button_sound.png')}
+                             className="btn-sound" alt=""/>
+                    </li>
+                );
+            }
+
+            if(this.state.feedbackTotal.recommendSentences) {
+                this.state.recommendSentList = this.state.feedbackTotal.recommendSentences.map((qna, i) =>
+                    <li className="recommend-sentence">
+                        <p>Q. {qna.question}</p>
+                        <p>A. {qna.answer}</p>
+                    </li>
+                );
+
+            }
+
+        }
 
         return (
             <div>
                 <div className="top-box">
                     <div className="feedback-date">
-                        {feedback.studyroomDate}
+                        {this.state.feedback.studyroomDate}
                     </div>
                     <div className="feedback-title">
-                        {feedback.studyroomTitle}
+                        {this.state.feedback.studyroomTitle}
                     </div>
                     <img src={require('../home/img/ic-home-character@3x.png')}
                          className="ic_Home_Character" alt=""/>
@@ -165,14 +120,14 @@ class FeedbackContent extends Component {
                             토스트가 추천하는 표현
                         </p>
                         <div className="box-content">
-                            <ul className="recommend-sentence-list">{recommendSentList}</ul>
+                            <ul className="recommend-sentence-list">{this.state.ecommendSentList}</ul>
                         </div>
 
                         <p className="box-title">
                             내가 실수했던 발음들 Check!
                         </p>
                         <div className="box-content">
-                            <ul className="missed-pronunciation-list">{missedPronunciationList}</ul>
+                            <ul className="missed-pronunciation-list">{this.state.missedPronunciationList}</ul>
                         </div>
 
                     </div>
